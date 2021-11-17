4ee649cf661cbcd6fb12abe8fde03962884707e5
Nikolas Laus <bchik3@gmail.com>
2021-11-17 13:28:15 +0200

    Initial commit.

---
+++ src/main/java/ee/bredbrains/phonebook/controller/ContactController.java
@@ -0,0 +1,35 @@
+package ee.bredbrains.phonebook.controller;
+
+import ee.bredbrains.phonebook.exception.UserNotFoundException;
+import ee.bredbrains.phonebook.model.Contact;
+import ee.bredbrains.phonebook.repository.ContactRepository;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/contact")
+public class ContactController {
+    private final ContactRepository repository;
+
+    public ContactController(ContactRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Contact> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Contact one(@PathVariable String id) {
+        long parsedId = Long.parseLong(id);
+        Optional<Contact> contact = repository.findById(parsedId);
+        return contact.orElseThrow(() -> new UserNotFoundException(parsedId));
+    }
+}
---
+++ src/test/java/ee/bredbrains/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bredbrains.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
