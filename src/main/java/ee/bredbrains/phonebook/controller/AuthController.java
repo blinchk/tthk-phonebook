9201612ca4ff25ac48fd226817a88892aa1780d2
Nikolas Laus <bchik3@gmail.com>
2021-11-19 09:35:05 +0200

    PHON-30 Add AuthController

---
+++ src/main/java/ee/bredbrains/phonebook/controller/AuthController.java
@@ -0,0 +1,9 @@
+package ee.bredbrains.phonebook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+}
