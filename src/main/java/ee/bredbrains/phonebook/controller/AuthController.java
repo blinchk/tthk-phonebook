ffec9e42320c76697a4cabc7157cd961320f7f79
Nikolas Laus <bchik3@gmail.com>
2021-11-19 09:42:46 +0200

    PHON-30 Add /login and /register endpoints

--- src/main/java/ee/bredbrains/phonebook/controller/AuthController.java
+++ src/main/java/ee/bredbrains/phonebook/controller/AuthController.java
@@ -1,9 +1,21 @@
 package ee.bredbrains.phonebook.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @GetMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String login() {
+        return "success";
+    }
+
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String register() {
+        return "success";
+    }
 }
