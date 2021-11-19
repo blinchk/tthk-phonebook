1239805c3fc43f80669d188f136c55b893549adc
Nikolas Laus <bchik3@gmail.com>
2021-11-19 09:11:11 +0200

    PHON-25 Add User model (#6)
    * PHON-25 Add User model and initialize UserController.
    
    * PHON-25 Add UserRepository
    
    * PHON-25 Convert Table to Entity

--- src/main/java/ee/bredbrains/phonebook/model/User.java
+++ src/main/java/ee/bredbrains/phonebook/model/User.java
@@ -1,5 +1,45 @@
 package ee.bredbrains.phonebook.model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
