918df07615b570d03fe124ccace23819031c6c51
Nikolas Laus <bchik3@gmail.com>
2021-11-22 14:52:27 +0200

    Update Gradle wrapper, add UserRepository to AuthController.

--- src/main/java/ee/bredbrains/phonebook/controller/AuthController.java
+++ src/main/java/ee/bredbrains/phonebook/controller/AuthController.java
@@ -1,5 +1,6 @@
 package ee.bredbrains.phonebook.controller;
 
+import ee.bredbrains.phonebook.repository.UserRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserRepository repository;
+
+    public AuthController(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public String login() {
         return "success";
