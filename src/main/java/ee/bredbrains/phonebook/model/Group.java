0ad52f9973db5bc85471e9d50e0bb4441c8e2e3b
Roasteg <krostislav468@gmail.com>
2021-12-01 19:59:17 +0200

    Hotfix CascadeType.MERGE

--- src/main/java/ee/bredbrains/phonebook/model/Group.java
+++ src/main/java/ee/bredbrains/phonebook/model/Group.java
@@ -14,7 +14,7 @@ public class Group {
     @Column
     private String title;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne(cascade = {CascadeType.MERGE})
     private User createdBy;
 
     @OneToMany(mappedBy = "group")
